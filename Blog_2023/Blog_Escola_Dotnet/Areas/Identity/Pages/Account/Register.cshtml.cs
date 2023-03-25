cf49fff70700080b56a9adcd81f9872f9bd33b88
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-25 02:17:17 -0300

    Navegação de login atualizado

--- Blog_2023/Blog_Escola_Dotnet/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Blog_2023/Blog_Escola_Dotnet/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -123,8 +123,8 @@ namespace Blog_Escola_Dotnet.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-                await _userStore.SetUserNameAsync(user, Input.FirsName, CancellationToken.None);
-                await _userStore.SetUserNameAsync(user, Input.LastName, CancellationToken.None);
+                user.FirstName = Input.FirsName;
+                user.LastName = Input.LastName;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
